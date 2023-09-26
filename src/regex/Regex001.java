801f27900a471432fdb454c5d206f38523503df9
ewaost <ewaostromecka@hotmail.com>
2023-09-26 18:59:43 +0200

    Refactor

--- src/regex/Regex001.java
+++ src/regex/Regex001.java
@@ -8,9 +8,8 @@ public class Regex001 {
 Napisz Regex, który sprawdza polskie numery telefonów komórkowych,
 zaczynające się cyfrą 5.
  */
-        private static final Pattern PATTERN = Pattern.compile("5[0-9]{8}");
+    private static final Pattern PATTERN = Pattern.compile("5[0-9]{8}");
 
-        public static void main(String[] args) {
         /*
         Prawda dla tekstu: 515332123
         Prawda dla tekstu: 515336293
@@ -20,21 +19,20 @@ zaczynające się cyfrą 5.
         Fałsz dla tekstu: 712123
         Fałsz dla tekstu: 51123
          */
-            validate("515332123");
-            validate("515336293");
-            validate("415336293");
-            validate("615332123");
-            validate("715332123");
-            validate("712123");
-            validate("51123");
-        }
 
-        public static void validate(String text){
-            Matcher matcher = PATTERN.matcher(text);
-            if(matcher.matches()){
-                System.out.println("Prawda dla tekstu: " + text);
-            } else {
-                System.out.println("Fałsz dla tekstu: " + text);
-            }
-        }
+
+    public static void main(String[] args) {
+        RegexTester tester = new RegexTester("(\\+48)?5[0-9]{8}");
+        tester.validate("+48515123456"); //true
+        tester.validate("+48715123456"); //false
+        tester.validate("515332123");
+        tester.validate("515336293");
+        tester.validate("415336293");
+        tester.validate("615332123");
+        tester.validate("715332123");
+        tester.validate("712123");
+        tester.validate("51123");
+    }
 }
+
+
